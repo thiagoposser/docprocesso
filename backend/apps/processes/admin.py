fa1cb61ff25250ce1c85d4a4212b716fa73292dd
Thiago Posser da Silva <thiagoposser@yahoo.com.br>
2026-08-11 16:31:44 -0300

    feat: add immutable process movements

--- backend/apps/processes/admin.py
+++ backend/apps/processes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import AdministrativeProcess, ProcessType
+from .models import AdministrativeProcess, ProcessMovement, ProcessType
 
 
 @admin.register(ProcessType)
@@ -21,6 +21,26 @@ class AdministrativeProcessAdmin(admin.ModelAdmin):
     search_fields = ("number", "title", "description")
     autocomplete_fields = ("process_type", "created_by", "origin_sector", "current_sector", "assignee")
     readonly_fields = ("number", "version", "created_at", "updated_at")
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(ProcessMovement)
+class ProcessMovementAdmin(admin.ModelAdmin):
+    list_display = ("process", "action", "from_sector", "to_sector", "actor", "created_at")
+    list_filter = ("action", "from_sector", "to_sector")
+    search_fields = ("process__number", "process__title", "actor__username", "note")
+    readonly_fields = (
+        "process", "action", "from_sector", "to_sector", "actor", "note",
+        "status_before", "status_after", "created_at",
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
     def has_delete_permission(self, request, obj=None):
         return False
